1aefae82917d3bb1ce16266a7c49d3719f0a33b6
dicristomanuel <dicristomanuel@icloud.com>
2015-05-14 08:34:32 -0700

    Created script for random colors

--- js/script.js
+++ js/script.js
@@ -1,27 +1,7 @@
 // Your JS goes here
 
 var win = new Audio(['http://www.freshly-ground.com/misc/music/20060826%20-%20Armstrong.mp3']);
-win.play();
-
-
-function redBlack() {
-	for (var i=0; i < 99; i++) {
-		var tile = document.createElement("div");
-		var body = document.querySelector("body");
-		body.appendChild(tile);
-		tile.style.width = "11.1%";
-		tile.style.float = "left";
-		tile.style.paddingBottom = "11.1%";
-			if(i%2 === 0) {
-				tile.style.background = "red";
-			} else {
-				tile.style.background = "black";
-			} //if statement
-	} //for loop
-} //redBlack
-
-//redBlack();
-
+	win.play();
 
 
 var colorSelector = function () {
@@ -46,43 +26,7 @@ function randomColor() {
 	} //for loop
 } //randomColor
 
-//randomColor();
+randomColor();
 
 
 
-// function gradient() {
-
-// 	for (var i=0; i < 99; i++) {
-// 		var tile = document.createElement("div");
-// 		var body = document.querySelector("body");
-// 		body.appendChild(tile);
-// 		tile.style.width = "11.1%";
-// 		tile.style.float = "left";
-// 		tile.style.paddingBottom = "11.1%";
-
-// 			if(i%2 === 0) {
-// 				tile.style.background = "skyblue";
-// 			} else {
-// 				tile.style.background = "hotpink";
-// 			} //if statement
-
-// 	} //for loop
-// } //gradient
-
-//gradient();
-
-function request() {
-	var toDo = prompt("Static, random or fading?");
-		if (toDo.toLowerCase() === "static") {
-			redBlack();
-		} else if (toDo.toLowerCase() === "random") {
-			randomColor();
-		} else if (toDo.toLowerCase() === "fading") {
-			alert("couldn't solve that one sorry!")
-		} else {
-		 	alert("Please type one of the choices");	 	
-		 	request();
-		} //if statement
-} //end request
-
-request();
